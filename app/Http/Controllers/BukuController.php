f7c4c739eb5845a59ee52df76ab085c8927ace5c
bryaniqbale <bryaniqbale@gmail.com>
2021-05-29 16:08:18 +0800

    Editan Baru

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class BukuController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $buku = Buku::all();
+        return view('tampilan0253', ['bukuadata' => $buku]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\AnggotaController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::resource('tampilan0253', AnggotaController::class);
+
+Route::resource('liketampilan0253', LikeController::class);
